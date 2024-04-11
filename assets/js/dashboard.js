2bb87af24d4871c89ffbe553a556969794698887
vishal singh <vk788043@gmail.com>
2024-04-11 22:02:10 +0530

    change in dashboard page.

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -4,6 +4,7 @@ let btn= document.getElementById("btn");
 let abs= document.getElementById("abs");
 let btnn= document.getElementsByClassName("btnn");
 let drop= document.getElementsByClassName("drop");
+let changeArrow= document.getElementsByClassName("changeArrow");
 
 btn.addEventListener("click", (e)=>{
     e.stopPropagation();
@@ -19,12 +20,15 @@ for(let i=0; i<btnn.length; i++){
         e.stopPropagation();
         if(drop[i].style.display=="block"){
             drop[i].style.display="none";
+            changeArrow[i].innerHTML=`<i class="fa-solid fa-angle-right"></i>`
         }else{
             drop[i].style.display="block";
+            changeArrow[i].innerHTML=`<i class="fa-solid fa-angle-down"></i>`
         }
     }
     document.addEventListener("click", ()=>{
         drop[i].style.display="none";
+        changeArrow[i].innerHTML=`<i class="fa-solid fa-angle-right"></i>`
     })
 }
 
