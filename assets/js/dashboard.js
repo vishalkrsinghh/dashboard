6460566dd1c4ea0eab63d16c99b2458e56b7a75d
vishal singh <vk788043@gmail.com>
2024-04-11 21:47:07 +0530

    some minor changes in design

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,6 +2,8 @@
 
 let btn= document.getElementById("btn");
 let abs= document.getElementById("abs");
+let btnn= document.getElementsByClassName("btnn");
+let drop= document.getElementsByClassName("drop");
 
 btn.addEventListener("click", (e)=>{
     e.stopPropagation();
@@ -11,6 +13,21 @@ btn.addEventListener("click", (e)=>{
         abs.style.display="block";
     }
 })
+
+for(let i=0; i<btnn.length; i++){
+    btnn[i].onclick= (e)=>{
+        e.stopPropagation();
+        if(drop[i].style.display=="block"){
+            drop[i].style.display="none";
+        }else{
+            drop[i].style.display="block";
+        }
+    }
+    document.addEventListener("click", ()=>{
+        drop[i].style.display="none";
+    })
+}
+
 document.addEventListener("click", ()=>{
     abs.style.display="none";
 })
